f2a3952990a9d1cb5e073169b7ec8faeadd3c554
Nikorasu <nikorasu85@gmail.com>
2023-01-17 02:31:42 -0600

    Simplified keypress method, added Katakana toggle.

--- matrix.py
+++ matrix.py
@@ -7,6 +7,7 @@ COLOR = ['\x1b[32m','\x1b[92m'] # green
 HIGHLIGHT = '\x1b[97m' # white
 DENSITY = 0.80 # percentage of terminal width to fill (default 0.80)
 MOVERATE = 0.1 # seconds between updates (default 0.1) lower is faster
+KANA = True # whether to include Japanese Katakana characters (default True)
 
 import random, os, string, time, sys, tty, termios, select
 
@@ -16,7 +17,8 @@ class MatrixColumn:
         self.start = -random.randint(0,os.get_terminal_size().lines) # random start position
         self.end = self.start-random.randint(3,os.get_terminal_size().lines) # random end position
         self.speed = random.choice([1,1,2]) # 1/3 chance of double speed
-        self.characters = 'ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ'+string.printable.strip() # characters to use
+        kata = 'ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ' if KANA else '' # katakana characters
+        self.characters = string.printable.strip() + kata # possible characters to use
         self.prechar = ''
         self.done = False
     def update(self):
@@ -37,38 +39,31 @@ class MatrixColumn:
         self.end += self.speed
         if self.end > termH: self.done = True # if end is off screen
 
-class NonBlockingInput:
-    def __enter__(self):
-        self.oldsettings = termios.tcgetattr(sys.stdin)
-        tty.setcbreak(sys.stdin) #.fileno()
-        return self
-    def __exit__(self,type,val,tp):
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.oldsettings)
-    def keypress(self):
-        if select.select([sys.stdin],[],[],0) == ([sys.stdin],[],[]):
-            return sys.stdin.read(1)
-        return False
-
 def main():
-    chains = []
+    running, chains = True, []
     taken = set() # set of unused columns
     print('\x1b[2J\x1b[?25l') # clear screen and hide cursor
     try:
-        with NonBlockingInput() as nbi: # enables non-blocking input
-            while nbi.keypress() not in ('\x1b', 'q'): # main loop, press ESC or q to quit
-                termW = os.get_terminal_size().columns
-                for i in range(int(termW*DENSITY)-len(chains)): # fill Density% of the terminal width with MatrixColumns
-                    while (column := random.randint(1,termW)) in taken: pass # ensures no overlappping columns (inefficient)
-                    chains.append(MatrixColumn(column)) # spawn MatrixColumn at unused column, add to list for updating
-                    taken.add(column) # add column to taken set
-                for mcol in chains: # loop through all MatrixColumns
-                    mcol.update() # run update function in each MatrixColumn
-                    if mcol.done: # remove MatrixColumns when they finish falling
-                        taken.remove(mcol.column) # remove column from taken set
-                        chains.remove(mcol) # then remove it from update list
-                time.sleep(MOVERATE) # controls the speed of the animation
+        oldsettings = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin)
+        while running: # main loop
+            termW = os.get_terminal_size().columns
+            for i in range(int(termW*DENSITY)-len(chains)): # fill Density% of the terminal width with MatrixColumns
+                while (column := random.randint(1,termW)) in taken: pass # prevents overlapping columns, inefficient
+                chains.append(MatrixColumn(column)) # spawn MatrixColumn at unused column, add to list for updating
+                taken.add(column) # add column to taken set
+            for mcol in chains: # loop through all MatrixColumns
+                mcol.update() # run update function in each MatrixColumn
+                if mcol.done: # remove MatrixColumns when they finish falling
+                    taken.remove(mcol.column) # remove column from taken set
+                    chains.remove(mcol) # then remove it from update list
+            time.sleep(MOVERATE) # controls the speed of the animation
+            if select.select([sys.stdin],[],[],0) == ([sys.stdin],[],[]): # check for input
+                if sys.stdin.read(1) in ('\x1b', 'q'): running = False # quit if ESC or q was pressed
     except KeyboardInterrupt: pass # catch Ctrl+C
-    finally: print('\x1b[0m\x1b[2J\x1b[?25h') # reset terminal and show cursor
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldsettings)
+        print('\x1b[0m\x1b[2J\x1b[?25h') # reset terminal and show cursor
 
 if __name__ == '__main__':
     main() # by Nik
